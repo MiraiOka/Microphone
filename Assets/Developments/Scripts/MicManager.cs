e875ec6f3e312bf5fd7f0eae402ef691b7721cb7
MiraOka <uaofmsbct.9g@gmail.com>
2018-10-31 21:24:43 +0900

    send volume

---
+++ Assets/Developments/Scripts/MicManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using uOSC;
+
+public class MicManager : MonoBehaviour {
+
+    AudioSource audio;
+
+
+	void Start () {
+        audio = GetComponent<AudioSource>();
+        audio.clip = Microphone.Start(null, true, 1, 44100);
+        audio.Play();
+	}
+	
+	void Update () {
+        uOscClient client = GetComponent<uOscClient>();
+
+        float[] data = new float[256];
+        float vol = 0;
+        audio.GetOutputData(data, 0);
+
+        foreach(float s in data)
+        {
+            vol += Mathf.Abs(s);
+        }
+        client.Send("", vol);
+	}
+}
